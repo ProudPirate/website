4bb88edf214b0acaa71fb60174189a73b31faf46
ProudPirate <shrestha.uts@gmail.com>
2019-09-06 13:33:42 +0530

    staticfiles

--- website/settings.py
+++ website/settings.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-import django_heroku
 import os
+import django_heroku
 
 # from django.conf.urls.static import static
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), '/website/static/', ]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), '/website/static/', ]
 # STATIC_ROOT = '/website/static/'
 
 # Quick-start development settings - unsuitable for production
