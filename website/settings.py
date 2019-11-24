13015e82b88cfea892797aa5f9237c89f9604497
ProudPirate <shrestha.uts@gmail.com>
2019-11-24 20:03:27 +0530

    UI Mods - Heroku Remove

--- website/settings.py
+++ website/settings.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
+
 import os
-import django_heroku
 
 # from django.conf.urls.static import static
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), '/website/static/', ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), '/website/static/', ]
 # STATIC_ROOT = '/website/static/'
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +31,6 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '192.168.0.102',
     '127.0.0.1',
-    'https://myroutine.herokuapp.com/',
 ]
 
 # Application definition
@@ -121,13 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     "/website/static",
-# ]
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    "/website/static"
+]
